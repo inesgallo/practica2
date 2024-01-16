ddffd6959508ddc7a89864857b9b2d29d0c787ed
carovasq <42carovasq@gmail.com>
2024-01-16 09:12:42 +0100

    add more let js
    add 4 lets ft js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-function addRow(){
-    // Declaramos 4 variables: A 3, le damos el valor de los input con los id's y a 1, crea una fila y se almacenan los valores de las 3 variables anteriores en cada celda de dicha fila
-    let name = document.getElementById('name').value;
-    let firstSurname = document.getElementById('firstSurname').value;
-    let secondSurname = document.getElementById('secondSurname').value;
-    let row = "<tr><td>" + name + "</td><td>" + firstSurname + "</td><td>" + secondSurname + "</td></tr>";
+function addRow() {
+    // Declaramos variables: A las primeras, le damos el valor de los input con los id's y a la última, se crea una fila y se almacenan los valores de las 3 variables anteriores en cada celda de dicha fila
+
+    var name = document.getElementById('name').value;
+    var surname1 = document.getElementById('surname1').value;
+    var surname2 = document.getElementById('surname2').value;
+    var role = document.getElementById('role').value;
+    var course = document.getElementById('course').value;
+    var classValue = document.getElementById('class').value;
+    var email = document.getElementById('email').value;
+
+    var row = "<tr><td>" + name + "</td><td>" + surname1 + "</td><td>" + surname2 + "</td><td>" + role + "</td><td>" + course + "</td><td>" + classValue + "</td><td>" + email + "</td></tr>";
+
     // Se agrega la fila anterior de la table con el id y nos situaciomos dentro del tag 'tbody'.
-    document.getElementById('tables').getElementsByTagName('tbody')[0].innerHTML += row;
+    document.getElementById('myTable').getElementsByTagName('tbody')[0].innerHTML += row;
+
     // Por último, se limpia los valores del Form
     document.getElementById('myForm').reset();
 }
-
-addRow();

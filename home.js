33b51f668f4235a69c9d618c2e9f8dbd14f3ba40
Yamila <yamilarobles10@gmail.com>
2024-01-18 17:27:21 +0100

    Table changes

--- home.js
+++ home.js
@@ -3,7 +3,6 @@ const form = document.getElementById('ourForm');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    let inputs = document.getElementsByTagName('input').value;
     const name = document.getElementById('inputName').value;
     const surname1 = document.getElementById('inputSurname1').value;
     const surname2 = document.getElementById('inputSurname2').value;
@@ -11,10 +10,14 @@ form.addEventListener('submit', function(e) {
     const house = document.getElementById('selectCasa').value;
     const course = document.getElementById('inputCurso').value;
     const classValue = document.getElementById('inputClase').value;
-
+    const buttonDelete = document.getElementById('buttonDelete');
+    //se crea una nueva fila de celdas en la tabla
+    buttonDelete.classList.remove('d-none');
+    let row = "<tr><td>" + name + "</td><td>" + surname1 + "</td><td>" + surname2 + "</td><td>" + email + "</td><td>" + house + "</td><td>" + course + "</td><td>" + classValue ;
     
-    let row = "<tr><td>" + name + "</td><td>" + surname1 + "</td><td>" + surname2 + "</td><td>" + email + "</td><td>" + house + "</td><td>" + course + "</td><td>" + classValue + "</td></tr>";
     
-    console.log("Fila creada:", row);
+    //limpiar form
+    form.reset();
+    
     document.getElementById('ourTable').getElementsByTagName('tbody')[0].innerHTML += row;
 });

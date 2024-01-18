d226cfc4177fbda0dbc676ed3738af2901f32eb6
Yamila <yamilarobles10@gmail.com>
2024-01-18 10:21:30 +0100

    Changes on form select

---
+++ home.js
@@ -0,0 +1,20 @@
+const formulario = document.querySelector('form');
+const nombre = document.querySelector('#inputName').value;
+const apellido1 = document.querySelector('#inputSurname1').value;
+const apellido2 = document.querySelector('#inputSurname2').value;
+const correo = document.querySelector('#inputEmail').value;
+const casa = document.querySelector('#selectCasa').value;
+const curso = document.querySelector('#inputCurso').value;
+const clase = document.querySelector('#inputClase').value;
+
+
+
+
+function getValueForm (){
+
+}
+
+
+formulario.addEventListener('submit', getValueForm);
+
+

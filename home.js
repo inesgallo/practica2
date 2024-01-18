b93cf9b7bafbf27f7e036e3d285898e77ae772f9
inesgallo <anainesgf@gmail.com>
2024-01-18 14:36:10 +0100

    update form function

--- home.js
+++ home.js
@@ -1,26 +1,20 @@
-
-function addRow (){
-    let nombre = document.getElementById('inputName').value;
-    let surname1 = document.getElementById('inputSurname1').value;
-    let surname2 = document.getElementById('inputSurname2').value;
-    let email = document.getElementById('inputEmail').value;
-    let home = document.getElementById('selectCasa').value;
-    let course = document.getElementById('inputCurso').value;
-    let classValue = document.getElementById('inputClase').value;
+const form = document.getElementById('ourForm');
 
 
-    let row = "<tr><td>" + nombre + "</td><td>" + surname1 + "</td><td>" + surname2 + "</td><td>" + email + "</td><td>" + home + "</td><td>" + course + "</td><td>" + classValue + "</td></tr>";
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let inputs = document.getElementsByTagName('input').value;
+    const name = document.getElementById('inputName').value;
+    const surname1 = document.getElementById('inputSurname1').value;
+    const surname2 = document.getElementById('inputSurname2').value;
+    const email = document.getElementById('inputEmail').value;
+    const house = document.getElementById('selectCasa').value;
+    const course = document.getElementById('inputCurso').value;
+    const classValue = document.getElementById('inputClase').value;
+
+    
+    let row = "<tr><td>" + name + "</td><td>" + surname1 + "</td><td>" + surname2 + "</td><td>" + email + "</td><td>" + house + "</td><td>" + course + "</td><td>" + classValue + "</td></tr>";
     
     console.log("Fila creada:", row);
     document.getElementById('ourTable').getElementsByTagName('tbody')[0].innerHTML += row;
-
-    // Por último, se limpia los valores del Form
-    document.getElementById('ourForm').reset();
-}
-
-
-
-
- 
-
-
+});

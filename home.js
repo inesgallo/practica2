81d96ce887ecabc28cf141abbcd2028598b94026
Yamila <yamilarobles10@gmail.com>
2024-01-18 11:39:25 +0100

    Add function onclick for new row

--- home.js
+++ home.js
@@ -1,20 +1,26 @@
-const formulario = document.querySelector('form');
-const nombre = document.querySelector('#inputName').value;
-const apellido1 = document.querySelector('#inputSurname1').value;
-const apellido2 = document.querySelector('#inputSurname2').value;
-const correo = document.querySelector('#inputEmail').value;
-const casa = document.querySelector('#selectCasa').value;
-const curso = document.querySelector('#inputCurso').value;
-const clase = document.querySelector('#inputClase').value;
+
+function addRow (){
+    let nombre = document.getElementById('inputName').value;
+    let surname1 = document.getElementById('inputSurname1').value;
+    let surname2 = document.getElementById('inputSurname2').value;
+    let email = document.getElementById('inputEmail').value;
+    let home = document.getElementById('selectCasa').value;
+    let course = document.getElementById('inputCurso').value;
+    let classValue = document.getElementById('inputClase').value;
 
 
+    let row = "<tr><td>" + nombre + "</td><td>" + surname1 + "</td><td>" + surname2 + "</td><td>" + email + "</td><td>" + home + "</td><td>" + course + "</td><td>" + classValue + "</td></tr>";
+    
+    console.log("Fila creada:", row);
+    document.getElementById('ourTable').getElementsByTagName('tbody')[0].innerHTML += row;
 
-
-function getValueForm (){
-
+    // Por último, se limpia los valores del Form
+    document.getElementById('ourForm').reset();
 }
 
 
-formulario.addEventListener('submit', getValueForm);
+
+
+ 
 
 
